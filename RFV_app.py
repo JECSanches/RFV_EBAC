8ab77b66d2667c80583c9464b22fb5c132475b27
José Eduardo <62633214+JECSanches@users.noreply.github.com>
2025-05-13 19:45:59 -0300

    Update RFV_app.py

--- RFV_app.py
+++ RFV_app.py
@@ -81,7 +81,7 @@ def main():
 
     # Botão para carregar arquivo na aplicação
     st.sidebar.write("## Suba o arquivo")
-    data_file_1 = st.sidebar.file_uploader("Bank marketing data", type = ['csv','xlsx'])
+    data_file_1 = st.sidebar.file_uploader("Dados", type = ['csv','xlsx'])
 
     # Verifica se há conteúdo carregado na aplicação
     if (data_file_1 is not None):
